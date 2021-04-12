1331a2b643ad39a14a8c395fd0b6e406fd0e9f75
Ahmed Kaddoura <akadd8@gmail.com>
2021-04-12 15:35:02 -0700

    Create new view controllers

---
+++ Spoiled/FridgeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FridgeViewController.swift
+//  Spoiled
+//
+//  Created by Ahmed Kaddoura on 4/12/21.
+//
+
+import UIKit
+
+class FridgeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
