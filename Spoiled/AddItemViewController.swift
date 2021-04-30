18ebd3e7179eaea4b1605160df7571086331fc89
Jenny Tang <tangjenny1224@gmail.com>
2021-04-30 10:38:06 -0700

    fixed datepicker bug. Datepicker is working now

--- Spoiled/AddItemViewController.swift
+++ Spoiled/AddItemViewController.swift
@@ -8,76 +8,19 @@
 import UIKit
 import Parse
 
+
 class AddItemViewController: UIViewController{
     
     @IBOutlet weak var nameInput: UITextField!
-    @IBOutlet weak var expirationInput: UITextField!
-        
-
-    let datePicker = UIDatePicker()
+    
+    @IBOutlet weak var expirationInput: UIDatePicker!
     
 
-  
     func createDatePicker(){
-//        datePicker.translatesAutoresizingMaskIntoConstraints = false
-////        view.addSubview(datePicker)
-//        datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = false
-//        datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50).isActive = false
-//
-//
-        //toolbar
-        let toolbar = UIToolbar()
-        toolbar.sizeToFit()
-        
-        //bar button
-        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
-        
-        toolbar.setItems([doneBtn], animated:true)
-        //assign toolbar
-        expirationInput.inputAccessoryView = toolbar
-        // assign date picker to the text field
-        expirationInput.inputView = datePicker
-       
-
-//        // date picker mode
-        datePicker.preferredDatePickerStyle = .inline
-        datePicker.datePickerMode = .date
-        datePicker.sizeToFit()
+        expirationInput.datePickerMode = .date
+        expirationInput.preferredDatePickerStyle = .inline
 
     }
-    
-    
-    
-    
-//
-//    func createDatePicker(){
-//        datePicker.translatesAutoresizingMaskIntoConstraints = false
-////        view.addSubview(datePicker)
-//        datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = false
-//        datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50).isActive = false
-//
-//
-//        //toolbar
-//        let toolbar = UIToolbar()
-//        toolbar.sizeToFit()
-//
-//        //bar button
-//        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
-//
-//        toolbar.setItems([doneBtn], animated:true)
-//        //assign toolbar
-//        expirationInput.inputAccessoryView = toolbar
-//        // assign date picker to the text field
-//        expirationInput.inputView = datePicker
-//
-//
-////        // date picker mode
-//        datePicker.preferredDatePickerStyle = .compact
-//        datePicker.datePickerMode = .date
-//        datePicker.sizeToFit()
-//
-//    }
-//
     
 
     override func viewDidLoad() {
@@ -86,20 +29,19 @@ class AddItemViewController: UIViewController{
         // Do any additional setup after loading the view.
     }
 
-    @objc func donePressed(){
-        //formatter
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .none
-        
-        expirationInput.text = formatter.string(from: datePicker.date)
-        self.view.endEditing(true)
-    }
-    
+
     @IBAction func onAddIngredientButton(_ sender: Any) {
         let ingredient = PFObject(className: "Ingredients")
         ingredient["name"] = nameInput.text!
-        ingredient["expiration"] = expirationInput.text!
+        
+        
+        
+        let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/yyyy"
+        ingredient["expiration"] = dateFormatter.string(from: expirationInput.date)
+            self.view.endEditing(true)
+
+        
         ingredient["author"] = PFUser.current()
         
         ingredient.saveInBackground { (success, error) in
@@ -112,10 +54,7 @@ class AddItemViewController: UIViewController{
         }
     }
     
-    
-    
-    
-    
+
 
     /*
     // MARK: - Navigation
